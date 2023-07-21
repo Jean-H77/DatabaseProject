ea45a716b06b5c686986a85532b7cf80db3d848c
John <55899462+JohnRsps77@users.noreply.github.com>
2023-07-21 10:47:46 -0700

    Project setup

---
+++ src/main/java/org/db/Main.java
@@ -0,0 +1,22 @@
+package org.db;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        primaryStage.setTitle("App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
