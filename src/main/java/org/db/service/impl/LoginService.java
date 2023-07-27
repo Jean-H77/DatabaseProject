e90a237055a1396eb0147e6da47577a9c36b63f5
jyou10 <106636656+jyou10@users.noreply.github.com>
2023-07-26 17:02:33 -0700

    add login functionality

--- src/main/java/org/db/service/impl/LoginService.java
+++ src/main/java/org/db/service/impl/LoginService.java
@@ -3,6 +3,7 @@ package org.db.service.impl;
 import org.db.database.Database;
 import org.db.model.Details;
 import org.db.model.LoginDetails;
+import org.db.model.User;
 import org.db.service.AccountService;
 
 public class LoginService extends AccountService {
@@ -16,6 +17,17 @@ public class LoginService extends AccountService {
         if(!(details instanceof LoginDetails))
             return "Error while logging in.";
         LoginDetails loginDetails = (LoginDetails) details;
+
+        if(!database.getUser(loginDetails).isPresent()) {
+            return "Account not found.";
+        }
+
+        User user = database.getUser(loginDetails).get();
+
+        if(!(user.getUsername().equals(loginDetails.getUsername())) && (user.getPassword().equals(loginDetails.getPassword()))) {
+            return "Incorrect username or password.";
+        }
+
         return "Success";
     }
 }
