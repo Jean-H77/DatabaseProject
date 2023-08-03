02d06d8ab2c6e819a5af1e64d50e055ccaed5963
Gr00vyGrim <116598731+Gr00vyGrim@users.noreply.github.com>
2023-08-03 11:51:20 -0700

    Added new service class

--- src/main/java/org/db/service/impl/HomepageService.java
+++ src/main/java/org/db/service/impl/HomepageService.java
@@ -11,10 +11,7 @@ public class HomepageService extends Service {
         super(database);
     }
 
-    // here is where we will make the database call to insert new item
-
     public String validate(HomepageDetails homepageDetails){
-        // Check the number of posts made by the user on the current date
         int postCountToday = database.getPostCountForUserOnDate(homepageDetails.getUsername(), homepageDetails.getDate());
         if (postCountToday >= 3) {
             return"Daily post limit\n(3 posts per day).";
