6f5effdb7f04272bc8911f71d829848eab61a4e8
Gr00vyGrim <116598731+Gr00vyGrim@users.noreply.github.com>
2023-08-07 15:32:56 -0700

    Updated item and category tables

--- schema.sql
+++ schema.sql
@@ -9,12 +9,24 @@ CREATE TABLE IF NOT EXISTS users(
                                     UNIQUE(EMAIL)
 );
 
+
+CREATE TABLE IF NOT EXISTS categories (
+                                     CATEGORY_ID int NOT NULL AUTO_INCREMENT,
+                                     CATEGORY_NAME varchar(50) NOT NULL,
+                                     PRIMARY KEY (CATEGORY_ID)
+);
+
+INSERT INTO categories (CATEGORY_NAME) VALUES ('apparel'), ('appliances'), ('electronics');
+
 CREATE TABLE IF NOT EXISTS items (
-                                     ID INT AUTO_INCREMENT PRIMARY KEY,
-                                     TITLE VARCHAR(100) NOT NULL,
-                                     DESCRIPTION TEXT NOT NULL,
-                                     CATEGORY VARCHAR(50) NOT NULL,
-                                     PRICE DOUBLE NOT NULL,
-                                     USERNAME VARCHAR(50) NOT NULL,
-                                     POSTED_TIMESTAMP TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+									 ID INT NOT NULL AUTO_INCREMENT,
+									 TITLE varchar(100) NOT NULL,
+									 DESCRIPTION TEXT NOT NULL,
+									 CATEGORY_ID INT DEFAULT NULL,
+									 PRICE DOUBLE NOT NULL,
+									 USERNAME varchar(50) NOT NULL,
+									 POSTED_TIMESTAMP timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+									 PRIMARY KEY (ID),
+									 KEY category_id (category_id),
+									 CONSTRAINT items_fk FOREIGN KEY (category_id) REFERENCES categories (category_id)
 );
