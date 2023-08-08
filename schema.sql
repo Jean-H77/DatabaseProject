a7b2b31ab4ccd5fba785097c8f761c052aa77eca
Gr00vyGrim <116598731+Gr00vyGrim@users.noreply.github.com>
2023-08-08 12:49:46 -0700

    refactored

--- schema.sql
+++ schema.sql
@@ -1,40 +1,33 @@
 use comp440;
 
 CREATE TABLE IF NOT EXISTS users(
-                                    USERNAME VARCHAR(16) PRIMARY KEY NOT NULL,
-                                    PASSWORD VARCHAR(20) NOT NULL,
-                                    FIRST_NAME VARCHAR(255) NOT NULL,
-                                    LAST_NAME VARCHAR(255) NOT NULL,
-                                    EMAIL VARCHAR(255) NOT NULL,
-                                    UNIQUE(EMAIL)
+    USERNAME VARCHAR(16) PRIMARY KEY NOT NULL,
+    PASSWORD VARCHAR(20) NOT NULL,
+    FIRST_NAME VARCHAR(255) NOT NULL,
+    LAST_NAME VARCHAR(255) NOT NULL,
+    EMAIL VARCHAR(255) NOT NULL,
+    UNIQUE(EMAIL)
 );
 
-
 CREATE TABLE IF NOT EXISTS categories (
-                                     CATEGORY_ID int NOT NULL AUTO_INCREMENT,
-                                     CATEGORY_NAME varchar(50) NOT NULL,
-                                     PRIMARY KEY (CATEGORY_ID)
+    CATEGORY_ID int NOT NULL AUTO_INCREMENT,
+    CATEGORY_NAME varchar(50) NOT NULL,
+    PRIMARY KEY (CATEGORY_ID)
 );
 
 INSERT INTO categories (CATEGORY_NAME) VALUES ('Apparel'), ('Appliances'), ('Electronics'), ('Footwear'), ('Jewelry');
 
 CREATE TABLE IF NOT EXISTS items (
-									 ITEM_ID INT NOT NULL AUTO_INCREMENT,
-									 TITLE varchar(100) NOT NULL,
-									 DESCRIPTION TEXT NOT NULL,
-									 PRICE DOUBLE NOT NULL,
-									 USERNAME varchar(50) NOT NULL,
-									 POSTED_TIMESTAMP timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-									 PRIMARY KEY (ITEM_ID)
+    ITEM_ID INT NOT NULL AUTO_INCREMENT,
+    TITLE varchar(100) NOT NULL,
+    DESCRIPTION TEXT NOT NULL,
+    PRICE DOUBLE NOT NULL,
+    USERNAME varchar(50) NOT NULL,
+    POSTED_TIMESTAMP timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (ITEM_ID)
 );
 
-CREATE TABLE IF NOT EXISTS item_categories (
-                                     ITEM_ID INT NOT NULL,
-                                     CATEGORY_ID INT NOT NULL,
-                                     FOREIGN KEY (ITEM_ID) REFERENCES items (ITEM_ID),
-                                     FOREIGN KEY (CATEGORY_ID) REFERENCES categories (CATEGORY_ID)
-);
-
+-- types table
 CREATE TABLE IF NOT EXISTS types (
     TYPE_ID INT NOT NULL AUTO_INCREMENT,
     TYPE_NAME VARCHAR(50) NOT NULL,
@@ -43,7 +36,17 @@ CREATE TABLE IF NOT EXISTS types (
     FOREIGN KEY (CATEGORY_ID) REFERENCES categories (CATEGORY_ID)
 );
 
+-- item_categories table
+CREATE TABLE IF NOT EXISTS item_categories (
+    ITEM_ID INT NOT NULL,
+    TYPE_ID INT NOT NULL,
+    FOREIGN KEY (ITEM_ID) REFERENCES items (ITEM_ID),
+    FOREIGN KEY (TYPE_ID) REFERENCES types (TYPE_ID)
+);
 
+
+
+-- maker table
 CREATE TABLE IF NOT EXISTS makers (
     MAKER_ID INT NOT NULL AUTO_INCREMENT,
     MAKER_NAME VARCHAR(100) NOT NULL,
