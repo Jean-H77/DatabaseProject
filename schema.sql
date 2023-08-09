54c41eca38875c78d2f159d8cb02773cbbface23
jyou10 <106636656+jyou10@users.noreply.github.com>
2023-08-08 17:54:51 -0700

    fix schema

--- schema.sql
+++ schema.sql
@@ -74,13 +74,13 @@ INSERT INTO categoryMakers (MAKER_NAME, CATEGORY_ID) VALUES
 ('None',1), ('Nike', 1), ('LVMH', 1), ('Adidas', 1), ('PUMA', 1), ('Levi', 1), ('Gap', 1), ('J. Crew', 1), ('Guess', 1), ('True Religion', 1), ('Under Armour', 1),
 ('None',2), ('LG', 2), ('Panasonic', 2), ('Haier', 2), ('Gree', 2), ('Whirlpool', 2), ('Bosh-Siemens', 2), ('Electrolux AB', 2),
 ('None',3), ('Apple', 3), ('Samsung', 3), ('Microsoft', 3), ('Google', 3), ('Vivo', 3), ('Huawei', 3), ('Motorola', 3), ('Nokia', 3), ('LG', 3),
-('None',4, ('Nike', 4), ('Adidas', 4), ('Sketchers', 4), ('New Balance', 4), ('Under Armour', 4), ('Asics', 4), ('Vans', 4),
+('None',4), ('Nike', 4), ('Adidas', 4), ('Sketchers', 4), ('New Balance', 4), ('Under Armour', 4), ('Asics', 4), ('Vans', 4),
 ('None',5), ('Mejuri', 5), ('BaubleBar', 5), ('Blue Nile', 5), ('Brilliant Earth', 5), ('Vrai', 5), ('Net-A-Porter', 5), ('Gorjana', 5)
 ;
 
 INSERT INTO categoryTypes (TYPE_NAME, CATEGORY_ID) VALUES
 ('None',1), ('Shirt', 1), ('Pants', 1), ('Jacket', 1),
 ('None',2), ('Fridge', 2), ('Blender', 2), ('Washing machine', 2),
-('None',3, ('Phone', 3), ('TV', 3), ('Computer', 3),
-('None',4, ('Running', 4), ('Tennis', 4), ('Sandals', 4),
+('None',3), ('Phone', 3), ('TV', 3), ('Computer', 3),
+('None',4), ('Running', 4), ('Tennis', 4), ('Sandals', 4),
 ('None',5), ('Earring', 5), ('Bracelet', 5), ('Necklace', 5)
